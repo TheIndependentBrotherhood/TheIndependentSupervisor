a43bb7e2e0a4bfd220f00348676ab952814b34a3
The Independent Brotherhood <theindependentbrotherhood@gmail.com>
2018-12-03 21:42:37 +0100

    Adding Toolbar

--- MeanStack/AngularPrj/tib/src/app/app.module.ts
+++ MeanStack/AngularPrj/tib/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent
     , PostCreateComponent
+    , HeaderComponent
   ],
   imports: [
     BrowserModule
@@ -19,6 +21,7 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     , MatInputModule
     , MatCardModule
     , MatButtonModule
+    , MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
