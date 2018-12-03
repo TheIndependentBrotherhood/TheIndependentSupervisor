8e50bceee0b528770d7d3cabb8d231d6fba5b2b8
The Independent Brotherhood <theindependentbrotherhood@gmail.com>
2018-12-03 21:06:39 +0100

    Angular Material

--- MeanStack/AngularPrj/tib/src/app/posts/post-create/post-create.component.ts
+++ MeanStack/AngularPrj/tib/src/app/posts/post-create/post-create.component.ts
@@ -4,6 +4,7 @@ import { post } from 'selenium-webdriver/http';
 @Component({
   selector: 'app-post-create'
   , templateUrl: './post-create.component.html'
+  , styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
   enteredValue = '';
