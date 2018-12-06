8ebacef2b1ae90f354f27974d04df08d51d8deea
The Independent Brotherhood <theindependentbrotherhood@gmail.com>
2018-12-06 11:54:52 +0100

    Creating a Post Model

--- MeanStack/AngularPrj/tib/src/app/app.component.ts
+++ MeanStack/AngularPrj/tib/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Post } from './posts/post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  storedPosts = [];
+  storedPosts: Post[] = [];
 
   onPostAdded(post) {
+    // this.storedPosts.push(3); Generate an error in IDE because '3' isn't assigneable to the Post's model (Number != String)
     this.storedPosts.push(post);
   }
 }
