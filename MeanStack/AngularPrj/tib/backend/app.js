89fb0776e2ff1708990b0bd34a38a9cfd05b9988
TheIndependentBrotherhood <theindependentbrotherhood@gmail.com>
2019-01-22 21:16:24 +0100

    Deploying in a Single App

--- MeanStack/AngularPrj/tib/backend/app.js
+++ MeanStack/AngularPrj/tib/backend/app.js
@@ -17,15 +17,19 @@ mongoose.connect('mongodb://brotherhood:brotherhood@localhost/TIBDB', { useNewUr
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
-app.use("/images", express.static(path.join('backend/images')));
+app.use("/images", express.static(path.join(__dirname, 'images')));
+app.use("/", express.static(path.join(__dirname, 'angular')));
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   next();
-});
+}); */
 
 app.use("/api/posts", postsRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'angular', 'index.html'));
+});
 
 module.exports = app;
