620ddb7927318be9f826df58b58620374332e508
TheIndependentBrotherhood <theindependentbrotherhood@gmail.com>
2019-01-23 23:04:47 +0100

    Implementing SPA Authentification

--- MeanStack/AngularPrj/tib/backend/routes/user.js
+++ MeanStack/AngularPrj/tib/backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -24,6 +25,38 @@ router.post("/signup" , (req, res, next) => {
             error: err
           })
         });
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: 'Auth Failed!'
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: 'Auth Failed!'
+        });
+      }
+      const token = jwt.sign({
+        email: user.email
+        , userId : user._id
+      }
+      , 'le_secret_devrait_être_plus_long'
+      , {
+        expiresIn: '1h'
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: 'Auth Failed!'
+      });
     });
 });
 
