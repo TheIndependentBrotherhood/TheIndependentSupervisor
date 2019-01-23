b23f6b6420a243dfda07a75838fedfb35ac94e01
TheIndependentBrotherhood <theindependentbrotherhood@gmail.com>
2019-01-23 12:59:30 +0100

    Connecting Angular to the Backend

--- MeanStack/AngularPrj/tib/src/app/auth/signup/signup.component.ts
+++ MeanStack/AngularPrj/tib/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from '../auth.service';
+
 @Component({
   templateUrl: './signup.component.html'
   , styleUrls: ['./signup.component.css']
@@ -8,7 +10,12 @@ import { NgForm } from '@angular/forms';
 export class SignupComponent {
   isLoading = false;
 
-  onSignup(form: NgForm) {
+  constructor(public authService: AuthService) {}
 
+  onSignup(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.authService.createUser(form.value.email, form.value.password);
   }
 }
