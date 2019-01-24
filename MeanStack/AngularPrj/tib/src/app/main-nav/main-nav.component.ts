6d3332a18b4f56b411246c5e0ff07c419f87db46
TheIndependentBrotherhood <theindependentbrotherhood@gmail.com>
2019-01-24 23:51:39 +0100

    Improving the UI Header to Reflect the Authentication Status

--- MeanStack/AngularPrj/tib/src/app/main-nav/main-nav.component.ts
+++ MeanStack/AngularPrj/tib/src/app/main-nav/main-nav.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css'],
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit, OnDestroy {
+  private authListenerSubs: Subscription;
+  userIsAuthenticated = false;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
-    );
+  );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+    });
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+  }
 
 }
