60678dbd9a9ba342c6d061097624ca58dc703543
The Independent Brotherhood <theindependentbrotherhood@gmail.com>
2019-01-25 16:47:01 +0100

    Adding Route Guards

--- MeanStack/AngularPrj/tib/src/app/app-routing.module.ts
+++ MeanStack/AngularPrj/tib/src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: DataTableComponent }
-  , { path: 'create', component: PostCreateComponent }
+  , { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] }
   , { path: 'posts', component: PostListComponent }
-  , { path: 'edit/:postId', component: PostCreateComponent }
+  , { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard] }
   , { path: 'login', component: LoginComponent }
   , { path: 'signup', component: SignupComponent }
 
@@ -20,6 +21,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule {
 
