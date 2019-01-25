2a3589f1706386d6a6e0d7e90986445115205aec
The Independent Brotherhood <theindependentbrotherhood@gmail.com>
2019-01-26 00:02:29 +0100

    Adding the User ID to Posts

--- MeanStack/AngularPrj/tib/backend/routes/posts.js
+++ MeanStack/AngularPrj/tib/backend/routes/posts.js
@@ -30,9 +30,10 @@ const storage = multer.diskStorage({
 router.post('', checkAuth, multer({storage: storage}).single('image'), (req, res, next) => {
   const url = req.protocol + '://' + req.get('host');
   const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename
+    title: req.body.title
+    , content: req.body.content
+    , imagePath: url + "/images/" + req.file.filename
+    , creator: req.userData.userId
   });
   post.save().then(createdPost => {
     res.status(201).json({
