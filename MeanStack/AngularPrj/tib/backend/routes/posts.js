ff188cf90cd19a3c4fa9b426628db94d10816531
The Independent Brotherhood <theindependentbrotherhood@gmail.com>
2019-01-26 17:59:00 +0100

    Returning Error Messages on the Server

--- MeanStack/AngularPrj/tib/backend/routes/posts.js
+++ MeanStack/AngularPrj/tib/backend/routes/posts.js
@@ -43,6 +43,11 @@ router.post('', checkAuth, multer({storage: storage}).single('image'), (req, res
         id: createdPost._id
       }
     });
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: 'Creating a post failed!'
+    });
   });
 });
 
@@ -59,13 +64,19 @@ router.put('/:id', checkAuth, multer({storage: storage}).single('image'), (req, 
     , imagePath: imagePath
     , creator: req.userData.userId
   });
-  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
-    if (result.nModified > 0) {
-      res.status(200).json({ message: 'Update succesful!' });
-    } else {
-      res.status(401).json({ message: 'Not authorized!' });
-    }
-  });
+  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
+    .then(result => {
+      if (result.nModified > 0) {
+        res.status(200).json({ message: 'Update succesful!' });
+      } else {
+        res.status(401).json({ message: 'Not authorized!' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Couldn\'t update post!'
+      })
+    });
 });
 
 router.get('', (req, res, next) => {
@@ -89,27 +100,44 @@ router.get('', (req, res, next) => {
         , posts: fectchedPosts
         , maxPosts: count
       });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Fetching posts failed!'
+      });
     });
 });
 
 router.get('/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({message: 'Post not found!'});
-    }
-  });
+  Post.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({message: 'Post not found!'});
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Fetching post failed!'
+      });
+    });
 });
 
 router.delete('/:id', checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(result => {
-    if (result.n > 0) {
-      res.status(200).json({ message: 'Deletion succesful!' });
-    } else {
-      res.status(401).json({ message: 'Not authorized!' });
-    }
-  });
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
+    .then(result => {
+      if (result.n > 0) {
+        res.status(200).json({ message: 'Deletion succesful!' });
+      } else {
+        res.status(401).json({ message: 'Not authorized!' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Fetching posts failed!'
+      });
+    });
 });
 
 module.exports = router;
