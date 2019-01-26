51fc9e66c3b203c493eaad963856312d66d0627a
The Independent Brotherhood <theindependentbrotherhood@gmail.com>
2019-01-26 02:06:39 +0100

    Using the User ID on the Frontend

--- MeanStack/AngularPrj/tib/backend/routes/posts.js
+++ MeanStack/AngularPrj/tib/backend/routes/posts.js
@@ -57,6 +57,7 @@ router.put('/:id', checkAuth, multer({storage: storage}).single('image'), (req, 
     , title: req.body.title
     , content: req.body.content
     , imagePath: imagePath
+    , creator: req.userData.userId
   });
   Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
     if (result.nModified > 0) {
