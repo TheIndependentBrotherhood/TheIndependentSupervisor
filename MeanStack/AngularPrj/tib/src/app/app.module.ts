5379c050262ab02250f60bedb887238e915f4d17
The Independent Brotherhood <theindependentbrotherhood@gmail.com>
2019-01-27 20:13:24 +0100

    Creating the Auth Module

--- MeanStack/AngularPrj/tib/src/app/app.module.ts
+++ MeanStack/AngularPrj/tib/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { LayoutModule } from '@angular/cdk/layout';
@@ -10,8 +9,6 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { DataTableComponent } from './data-table/data-table.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { ErrorComponent } from './error/error.component';
 
 import { AuthInterceptor } from './auth/auth-interceptor';
@@ -19,6 +16,7 @@ import { ErrorInterceptor } from './error-interceptor';
 
 import { AngularMaterialModule } from './angular-material.module';
 import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
@@ -26,19 +24,17 @@ import { PostsModule } from './posts/posts.module';
     AppComponent
     , MainNavComponent
     , DataTableComponent
-    , LoginComponent
-    , SignupComponent
     , ErrorComponent
   ],
   imports: [
     BrowserModule
     , AppRoutingModule
-    , FormsModule
     , BrowserAnimationsModule
     , LayoutModule
     , HttpClientModule
     , AngularMaterialModule
     , PostsModule
+    , AuthModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
